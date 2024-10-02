00cd28dac619c15fbfc54803802bd006580bf243
Edualex0 <127694415+Edualex0@users.noreply.github.com>
2024-10-02 14:43:32 -0300

    Update Pedido.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Pedido.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Pedido.java
@@ -1,5 +1,26 @@
 package unicap.sistemasdegerenciamento.Restaurante;
 
 public class Pedido {
-    
-}
+    private int id;
+    private Mesa mesa;
+    private List<ItemDoPedido> itens;
+    private double total;
+
+    public Pedido(int id, Mesa mesa) {
+        this.id = id;
+        this.mesa = mesa;
+        this.itens = new ArrayList<>();
+        this.total = 0.0;
+    }
+
+    public void adicionarItem(ItemDoPedido item) {
+        itens.add(item);
+        total += item.getPreco();
+    }
+
+    public void removerItem(ItemDoPedido item) {
+        itens.remove(item);
+        total -= item.getPreco();
+    }
+
+// Ainda tá faltando coisa
