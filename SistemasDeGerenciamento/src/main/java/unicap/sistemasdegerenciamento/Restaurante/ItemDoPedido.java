6d6850544b83183f363ef339b5fab0d141927f51
Edualex0 <127694415+Edualex0@users.noreply.github.com>
2024-10-02 12:44:34 -0300

    Update ItemDoPedido.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/ItemDoPedido.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/ItemDoPedido.java
@@ -1,5 +1,19 @@
 package unicap.sistemasdegerenciamento.Restaurante;
 
 public class ItemDoPedido {
-    
+    private String nome;
+    private double preco;
+
+    public ItemDoPedido(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
 }
