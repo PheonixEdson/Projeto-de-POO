adc9aa2382e151b5896eaf21ec9a16a1134f1e82
DavidCands2525 <davidcandido1914@gmail.com>
2024-10-02 17:29:45 -0300

    Atualizando as classes ItemDoPedido e Pedido

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/ItemDoPedido.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/ItemDoPedido.java
@@ -2,15 +2,25 @@ package unicap.sistemasdegerenciamento.Restaurante;
 
 public class ItemDoPedido {
     private String nome;
+    private int quantidade;
     private double preco;
 
-    public ItemDoPedido(String nome, double preco) {
+    public ItemDoPedido(String nome, int quantidade, double preco) {
         this.nome = nome;
+        this.quantidade = quantidade;
         this.preco = preco;
     }
 
+    public double calcularPrecoTotal(){
+        return quantidade * preco;
+    }
+    
     public String getNome() {
         return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
     }
 
     public double getPreco() {
--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Pedido.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Pedido.java
@@ -1,26 +1,66 @@
 package unicap.sistemasdegerenciamento.Restaurante;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
     private int id;
     private Mesa mesa;
     private List<ItemDoPedido> itens;
     private double total;
+    private boolean fechado;
 
     public Pedido(int id, Mesa mesa) {
         this.id = id;
         this.mesa = mesa;
         this.itens = new ArrayList<>();
         this.total = 0.0;
+        this.fechado = false;
     }
 
-    public void adicionarItem(ItemDoPedido item) {
-        itens.add(item);
-        total += item.getPreco();
+    public void adicionarItem(ItemDoPedido item){
+        if(fechado == false){
+            itens.add(item);
+            total += item.calcularPrecoTotal();
+        }
+        else{
+            System.out.println("O pedido ja foi fechado!");
+        }
     }
 
-    public void removerItem(ItemDoPedido item) {
-        itens.remove(item);
-        total -= item.getPreco();
+    public void removerItem(ItemDoPedido item){
+        if(fechado == false){
+            itens.remove(item);
+            total -= item.calcularPrecoTotal();
+        }
+        else{
+            System.out.println("O pedido ja foi fechado!");
+        }
+    }
+    
+    public void fecharPedido(){
+        this.fechado = true;
+    }
+    
+    public void reabrirPedido(){
+        this.fechado = false;
+    }
+    
+    // Ainda tá faltando coisa
+
+    public int getId(){
+        return id;
     }
 
-// Ainda tá faltando coisa
+    public Mesa getMesa(){
+        return mesa;
+    }
+
+    public List<ItemDoPedido> getItens(){
+        return itens;
+    }
+
+    public boolean isFechado(){
+        return fechado;
+    }
+}
