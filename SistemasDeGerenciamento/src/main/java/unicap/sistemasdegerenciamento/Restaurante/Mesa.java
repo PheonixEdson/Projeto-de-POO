2f69134e4af4c820ae96a4dd300d6fa681fae652
DavidCands2525 <davidcandido1914@gmail.com>
2024-10-02 12:27:18 -0300

    Implementando a classe Mesa

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Mesa.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Mesa.java
@@ -1,5 +1,39 @@
 package unicap.sistemasdegerenciamento.Restaurante;
 
 public class Mesa {
+    private final int numero;
+    private Pedido pedido;
+    private boolean ocupada;
     
+    public Mesa(int numero){
+        this.numero = numero;
+        this.ocupada = false;
+        this.pedido = new Pedido();
+    }
+    
+    public void ocuparMesa(){
+        if(ocupada == false){
+            ocupada = true;
+        }
+        else{
+            System.out.println("A mesa está ocupada!");
+        }
+    }
+    
+    public void liberarMesa(){
+        ocupada = false;
+        pedido = new Pedido();
+    }
+
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public Pedido getPedido(){
+        return this.pedido;
+    }
+
+    public boolean isOcupada(){
+        return this.ocupada;
+    }
 }
--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Restaurante.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Restaurante.java
@@ -3,6 +3,9 @@ package unicap.sistemasdegerenciamento.Restaurante;
 public class Restaurante {
 
     public static void main(String[] args) {
+        Mesa mesa1 = new Mesa(1);
+        mesa1.ocuparMesa();
+        
         
     }
     
