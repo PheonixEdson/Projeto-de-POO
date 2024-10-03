e3b2fdcb97d2ef7c7e1a4de9517935afdd21911d
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2024-10-03 14:44:16 -0300

    Update Participante.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Eventos/Participante.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Eventos/Participante.java
@@ -2,12 +2,37 @@ package unicap.sistemasdegerenciamento.Eventos;
 
 class Participante {
     private String nome;
+    private String email;
+    private String telefone;
+    private int idade;
 
-    public Participante(String nome) {
+    public Participante(String nome, String email, String telefone, int idade) {
         this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.idade = idade;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Idade: " + idade);
     }
 }
