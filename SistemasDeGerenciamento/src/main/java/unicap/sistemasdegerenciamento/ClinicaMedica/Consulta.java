676b74df450fde641030ca61978046756cbf060c
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2024-10-10 15:55:49 -0300

    Update Consulta.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Consulta.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Consulta.java
@@ -1,5 +1,25 @@
 package unicap.sistemasdegerenciamento.ClinicaMedica;
+import java.util.Date;
 
 public class Consulta {
-    
+    private Paciente paciente;
+    private Medico medico;
+    private Date data;
+    public Consulta(Paciente paciente, Medico medico, Date data) {
+        this.paciente = paciente;
+        this.medico = medico;
+        this.data = data;
+    }
+    public Paciente getPaciente() {
+        return paciente;
+    }
+    public Medico getMedico() {
+        return medico;
+    }
+    public Date getData() {
+        return data;
+    }
+    public String toString() {
+        return "Consulta [Paciente: " + paciente.getNome() + ", Médico: " + medico.getNome() + ", Data: " + data + "]";
+    }
 }
