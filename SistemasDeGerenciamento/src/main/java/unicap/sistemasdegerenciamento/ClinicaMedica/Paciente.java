0d58e39cfeb386f42c0382210f04e99f0986515a
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2024-10-10 15:56:19 -0300

    Update Paciente.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Paciente.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Paciente.java
@@ -1,5 +1,16 @@
 package unicap.sistemasdegerenciamento.ClinicaMedica;
 
 public class Paciente {
-    
+    private String nome;
+    private String cpf;
+    public Paciente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
 }
