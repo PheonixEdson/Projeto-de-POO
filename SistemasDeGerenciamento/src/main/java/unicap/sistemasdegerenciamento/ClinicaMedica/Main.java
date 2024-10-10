b9ff37030f083d66a09fd20ea76b39cfc1e8c6b6
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2024-10-10 15:57:38 -0300

    Update and rename ClinicaMedica.java to Main.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Main.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class ClinicaMedica {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Clinica clinica = new Clinica();
