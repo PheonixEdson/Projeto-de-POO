a59a441c71cea181c626d8317cb4f8a27cfc0e25
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2024-10-25 09:59:44 -0300

    Update SistemaGerenciamentoRestaurante.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/SistemaGerenciamentoRestaurante.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/SistemaGerenciamentoRestaurante.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Restaurante {
  
     public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
         List<Mesa> mesas = new ArrayList<>();
         int cont = 1;
  
