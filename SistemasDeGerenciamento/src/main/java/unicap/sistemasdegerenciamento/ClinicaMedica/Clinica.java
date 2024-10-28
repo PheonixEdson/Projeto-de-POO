3e70761f5baf5807e6e063d006266632c9b3d5bd
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2024-10-28 15:49:36 -0300

    Update Clinica.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Clinica.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Clinica.java
@@ -41,7 +41,7 @@ public class Clinica {
         return null;
     }
     
-    /*public void agendarConsulta(Paciente paciente, Medico medico, Date data) {
+    public void agendarConsulta(Paciente paciente, Medico medico, Date data) {
         if (medico.isDisponivel()) {
             Consulta consulta = new Consulta(paciente, medico, data);
             consultas.add(consulta);
@@ -51,7 +51,7 @@ public class Clinica {
         else {
             System.out.println("Médico não está disponível.");
         }
-    }*/
+    }
     
     public void buscarConsultas() {
         System.out.println("Relatório de Consultas:");
@@ -59,4 +59,28 @@ public class Clinica {
             System.out.println(consulta);
         }
     }
+
+  public static void main(String[] args) {
+    Clinica clinica = new Clinica();
+
+
+    Paciente paciente1 = new Paciente("João Silva", "joao@email.com");
+    Paciente paciente2 = new Paciente("Maria Oliveira", "maria@email.com");
+    
+    clinica.adicionarPaciente(paciente1);
+    clinica.adicionarPaciente(paciente2);
+    
+
+    Medico medico1 = new Medico("Dr. Pedro", "Cardiologia");
+    Medico medico2 = new Medico("Dra. Ana", "Pediatria");
+
+    clinica.adicionarMedico(medico1);
+    clinica.adicionarMedico(medico2);
+    
+    Date dataConsulta1 = new Date(); 
+    clinica.agendarConsulta(paciente1, medico1, dataConsulta1);
+    
+    Date dataConsulta2 = new Date(System.currentTimeMillis() + 86400000); 
+    clinica.agendarConsulta(paciente2, medico2, dataConsulta2);
+    clinica.buscarConsultas();
 }
